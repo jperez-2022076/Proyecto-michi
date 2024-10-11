9392d13f5ec3431657335a343b372e9b74399c31
jperez-2022076 <jperez-2022076@kinal.edu.gt>
2024-10-11 14:38:18 -0600

    Nuevos campos a historialPv.2

--- src/model/HistorialPV.js
+++ src/model/HistorialPV.js
@@ -1,54 +1,53 @@
 'use strict'
 
-import { model, Schema } from "mongoose"
+import { model, Schema } from "mongoose";
 
-const historalPV = Schema(
-    {
-        persona:{
-            type: Schema.Types.ObjectId,
-            ref: 'Persona',
-            required: true
-        },
-        vehiculo:{
-            type: Schema.Types.ObjectId,
-            ref: 'Vehiculo',
-            required: true
-        },
-        usuario:{
-            type: Schema.Types.ObjectId,
-            ref: 'Usuario',
-            required: true
-        },
-        nombre:{
-            type:String,
-            default:""
-        },
-        DPI:{
-            type:String,
-           default:""
-        },
-        placa: {
-            type: String,
-             default:""
-          },
-        estado: {
-            type: String,
-            enum: ['E', 'S'],
-            default: 'E'
-        },
-        fecha: {
-            type: Date,
-            required: true
-        },
-        hora: {
-            type: String,
-            required: true
-        },
- 
+const historalPV = new Schema(
+  {
+    persona: {
+      type: Schema.Types.ObjectId,
+      ref: 'Persona',
+      required: true
     },
-    {
-        versionKey: false, // Aquí es donde debe ir
-    }
-)
+    vehiculo: {
+      type: Schema.Types.ObjectId,
+      ref: 'Vehiculo',
+      required: true
+    },
+    usuario: {
+      type: Schema.Types.ObjectId,
+      ref: 'Usuario',
+      required: true
+    },
+    nombre: {
+      type: String,
+      default: null // Por defecto será null en lugar de una cadena vacía
+    },
+    DPI: {
+      type: String,
+      default: null // Por defecto será null
+    },
+    placa: {
+      type: String,
+      default: null // Por defecto será null
+    },
+    estado: {
+      type: String,
+      enum: ['E', 'S'],
+      default: 'E'
+    },
+    fecha: {
+      type: Date,
+      required: true
+    },
+    hora: {
+      type: String,
+      required: true
+    },
+  },
+  {
+    versionKey: false, // Aquí es donde debe ir
+  }
+);
 
-export default model('HistorialPV', historalPV)
+export default model('HistorialPV', historalPV);
