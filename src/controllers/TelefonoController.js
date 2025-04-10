8d0598fa2617c0344eff7119ad749aa8d02c63aa
jperez-2022076 <jperez-2022076@kinal.edu.gt>
2025-04-09 20:34:25 -0600

    Ya puedo hacer lo de guardarlo en mi base de datos

--- src/controllers/TelefonoController.js
+++ src/controllers/TelefonoController.js
@@ -9,3 +9,23 @@ export const createTelefono = async (req, res) => {
     res.status(500).json({ message: 'Error al crear teléfono', error: err.message });
   }
 };
+
+
+export const getTelefonoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const telefono = await Telefono.findById(id)
+    .populate('datos.persona')
+    .populate('datos.vehiculo')
+    .populate('datos.usuario');
+
+    if (!telefono) {
+      return res.status(404).json({ message: 'Teléfono no encontrado' });
+    }
+
+    res.status(200).json(telefono);
+  } catch (err) {
+    res.status(500).json({ message: 'Error al buscar teléfono', error: err.message });
+  }
+};
--- src/routes/TelefonoRouter.js
+++ src/routes/TelefonoRouter.js
@@ -1,10 +1,11 @@
 'use strict'
 
 import { Router } from "express"
-import { createTelefono } from "../controllers/TelefonoController.js";
+import { createTelefono, getTelefonoById } from "../controllers/TelefonoController.js";
 
 const api = Router();
 
 api.post('/agregar' ,createTelefono)
+api.get('/listar/:id', getTelefonoById); 
 
 export default api;
