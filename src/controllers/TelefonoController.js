2210ec60bbf69ceaba7698fb757389dd1a73ab49
jperez-2022076 <jperez-2022076@kinal.edu.gt>
2025-04-09 20:41:38 -0600

    eliminar arraylist

--- src/controllers/TelefonoController.js
+++ src/controllers/TelefonoController.js
@@ -29,3 +29,27 @@ export const getTelefonoById = async (req, res) => {
     res.status(500).json({ message: 'Error al buscar teléfono', error: err.message });
   }
 };
+
+
+export const eliminarDatoDeTelefono = async (req, res) => {
+  try {
+    const { idTelefono, idDato } = req.params;
+
+    const telefono = await Telefono.findByIdAndUpdate(
+      idTelefono,
+      { $pull: { datos: { _id: idDato } } }, // elimina del array el que tenga ese _id
+      { new: true }
+    )
+      .populate('datos.persona')
+      .populate('datos.vehiculo')
+      .populate('datos.usuario');
+
+    if (!telefono) {
+      return res.status(404).json({ message: 'Teléfono no encontrado' });
+    }
+
+    res.status(200).json({ message: 'Dato eliminado con éxito', telefono });
+  } catch (err) {
+    res.status(500).json({ message: 'Error al eliminar dato', error: err.message });
+  }
+};
--- src/routes/TelefonoRouter.js
+++ src/routes/TelefonoRouter.js
@@ -1,11 +1,13 @@
 'use strict'
 
 import { Router } from "express"
-import { createTelefono, getTelefonoById } from "../controllers/TelefonoController.js";
+import { createTelefono, eliminarDatoDeTelefono, getTelefonoById } from "../controllers/TelefonoController.js";
 
 const api = Router();
 
 api.post('/agregar' ,createTelefono)
 api.get('/listar/:id', getTelefonoById); 
+api.delete('/eliminar/:idTelefono/:idDato', eliminarDatoDeTelefono);
+
 
 export default api;
